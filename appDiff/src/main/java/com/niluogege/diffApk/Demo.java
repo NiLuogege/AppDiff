bf5e9df47837f882859ba783182f7dcc363e10b9
luoChen <hank.luo@xianghuanji.com>
2019-12-20 13:12:59 +0800

    first commit

---
+++ appDiff/src/main/java/com/niluogege/diffApk/Demo.java
@@ -0,0 +1,15 @@
+package com.niluogege.diffApk;
+
+/**
+ * Created by niluogege on 2019/12/20.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        String[] a = {
+                "C:\\Users\\niluogege\\Desktop\\demo\\XHJ_V3.3.14_2019-11-12.apk",
+                "C:\\Users\\niluogege\\Desktop\\demo\\XHJ_V3.3.16_2019-12-16.apk",
+                "C:\\Users\\niluogege\\Desktop\\demo\\change"
+        };
+        Main.main(a);
+    }
+}
